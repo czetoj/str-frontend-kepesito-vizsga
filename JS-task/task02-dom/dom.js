3cd0c22ee58a25b99a27440603049f1399a410e6
czetoj <czeto.jozsef.0229@gmail.com>
2021-04-30 12:46:07 +0200

    1.0

--- JS-task/task02-dom/dom.js
+++ JS-task/task02-dom/dom.js
@@ -1,4 +1,22 @@
-function changeOuterLinks() {}
+function changeOuterLinks() { }
 
+const links = document.querySelectorAll('#link-list a');
+
+links.forEach(item => {
+    if (item.innerHTML.includes('outer-link')) {
+        item.setAttribute('target', '_blank');
+        const strong = document.createElement('strong');
+        strong.innerHTML = item.innerHTML;
+        item.appendChild(strong);
+    } else item
+});
+
+const navElement = document.getElementsByTagName('nav')[0];
+navElement.style.display = 'flex';
+navElement.style.flexDirection = 'column';
+navElement.style.width = '30%';
+navElement.style.margin = '0 auto';
+navElement.style.border = '1px solid blue';
+navElement.style.padding = '16px';
 
 export { changeOuterLinks };
